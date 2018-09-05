161050b9e59e51d2329d3c7f0669a2fe94492d3a
Shelly (6utt3rfly) <Emmpa@fake.com>
2018-09-05 11:55:40 -0700

    Prevent overwriting query array values unless they are orderedUUid columns

--- index.js
+++ index.js
@@ -131,10 +131,10 @@ module.exports = (bookshelf) => {
             options = this.mapKnexQuery(options, function (obj, stmt) {
                 if (!Buffer.isBuffer(stmt.value)) {
                     Object.keys(obj.orderedUuids).forEach((column) => {
-                        if (Array.isArray(stmt.value)) {
+                        if (Array.isArray(stmt.value) && (stmt.column === `${obj.tableName}.${column}` || stmt.column === column)) {
                             const stmtValues = [];
                             stmt.value.forEach((stmtValue) => {
-                                if ((stmt.column === `${obj.tableName}.${column}` || stmt.column === column) && stmtValue) {
+                                if (stmtValue) {
                                     const generatedId = bookshelf.Model.prefixedUuidToBinary(stmtValue,
                                         (obj.orderedUuids[column] ? obj.orderedUuids[column].length : null));
                                     stmtValues.push(generatedId);
